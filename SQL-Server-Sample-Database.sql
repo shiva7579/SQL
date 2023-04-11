52135cf79a68a1cebc5acc43c8a0813297c7beb0
shiva7579 <109517166+shiva7579@users.noreply.github.com>
2023-04-11 20:36:29 +0545

    Add files via upload

--- SQL-Server-Sample-Database.sql
+++ SQL-Server-Sample-Database.sql
@@ -195,6 +195,62 @@ insert into patient_logs values (1, to_date('04-03-2020','dd-mm-yyyy'), 500);
 insert into patient_logs values (3, to_date('20-01-2020','dd-mm-yyyy'), 450);
 insert into patient_logs values (1, to_date('04-03-2020','dd-mm-yyyy'), 600);
 
+--weather_table
+DROP TABLE IF EXISTS weather cascade;
+CREATE TABLE weather
+	(
+		id 					int 				primary key,
+		city 				varchar(50) not null,
+		temperature int 				not null,
+		day 				date				not null
+	);
+
+INSERT INTO weather VALUES
+	(1, 'London', -1, to_date('2021-01-01','yyyy-mm-dd')),
+	(2, 'London', -2, to_date('2021-01-02','yyyy-mm-dd')),
+	(3, 'London', 4, to_date('2021-01-03','yyyy-mm-dd')),
+	(4, 'London', 1, to_date('2021-01-04','yyyy-mm-dd')),
+	(5, 'London', -2, to_date('2021-01-05','yyyy-mm-dd')),
+	(6, 'London', -5, to_date('2021-01-06','yyyy-mm-dd')),
+	(7, 'London', -7, to_date('2021-01-07','yyyy-mm-dd')),
+	(8, 'London', 5, to_date('2021-01-08','yyyy-mm-dd')),
+	(9, 'London', -20, to_date('2021-01-09','yyyy-mm-dd')),
+	(10, 'London', 20, to_date('2021-01-10','yyyy-mm-dd')),
+	(11, 'London', 22, to_date('2021-01-11','yyyy-mm-dd')),
+	(12, 'London', -1, to_date('2021-01-12','yyyy-mm-dd')),
+	(13, 'London', -2, to_date('2021-01-13','yyyy-mm-dd')),
+	(14, 'London', -2, to_date('2021-01-14','yyyy-mm-dd')),
+	(15, 'London', -4, to_date('2021-01-15','yyyy-mm-dd')),
+	(16, 'London', -9, to_date('2021-01-16','yyyy-mm-dd')),
+	(17, 'London', 0, to_date('2021-01-17','yyyy-mm-dd')),
+	(18, 'London', -10, to_date('2021-01-18','yyyy-mm-dd')),
+	(19, 'London', -11, to_date('2021-01-19','yyyy-mm-dd')),
+	(20, 'London', -12, to_date('2021-01-20','yyyy-mm-dd')),
+	(21, 'London', -11, to_date('2021-01-21','yyyy-mm-dd'));
+
+--vw_weather_table
+CREATE OR REPLACE VIEW vw_weather AS
+SELECT city, temperature FROM weather;
+
+--orders_table
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders
+  (
+    order_id    varchar(20) primary key,
+    order_date  date        not null
+);
+
+ 
+INSERT INTO orders VALUES
+  ('ORD1001', to_date('2021-Jan-01','yyyy-mon-dd')),
+  ('ORD1002', to_date('2021-Feb-01','yyyy-mon-dd')),
+  ('ORD1003', to_date('2021-Feb-02','yyyy-mon-dd')),
+  ('ORD1004', to_date('2021-Feb-03','yyyy-mon-dd')),
+  ('ORD1005', to_date('2021-Mar-01','yyyy-mon-dd')),
+  ('ORD1006', to_date('2021-Jun-01','yyyy-mon-dd')),
+  ('ORD1007', to_date('2021-Dec-25','yyyy-mon-dd')),
+  ('ORD1008', to_date('2021-Dec-26','yyyy-mon-dd'));
+COMMIT;
 
 --1)Write a SQL Query to fetch all the duplicate records in a table.
 
@@ -383,10 +439,52 @@ GROUP BY months)
 SELECT t3.account_id,t3.months,t4.max_no_of_patients
 FROM t3 JOIN t4 ON t4.months=t3.months AND t4.max_no_of_patients=t3.rown
 
+--10. SQL Query to fetch “N” consecutive records where temperature is below zero
+--10a. when the table has a primary key
 
+select * from weather;
 
+WITH t1 AS(
+SELECT *,id-row_number() over(order by id) as id_idff
+FROM weather 
+WHERE temperature < 0),
+t2 AS(
+SELECT COUNT(*) OVER(PARTITION BY id_idff ORDER BY id_idff),
+*
+FROM t1)
+SELECT id,city,temperature,day FROM t2
+WHERE count>=3;
 
+--10b. When table does not have a primary key
+select * from vw_weather;
 
+WITH t0 AS(SELECT *,row_number() over() as id
+FROM vw_weather ),
+t1 AS(
+SELECT *,id-row_number() over(order by id) as id_idff
+FROM t0
+WHERE temperature < 0),
+t2 AS(
+SELECT COUNT(*) OVER(PARTITION BY id_idff ORDER BY id_idff),
+*
+FROM t1)
+SELECT city,temperature FROM t2
+WHERE count>=3;
+
+--10c. Query logic based on data field
+SELECT * FROM orders;
+WITH t1 AS(
+SELECT *,ROW_NUMBER() OVER(ORDER BY order_date) AS id
+FROM orders),
+t2 AS(
+SELECT order_date-(id::int) AS cons_date,*
+FROM t1),
+t3 AS(
+SELECT *,COUNT(cons_date) OVER(PARTITION BY cons_date ORDER BY cons_date) as count
+FROM t2)
+SELECT order_id,order_date
+FROM t3
+WHERE count>2;
 
 
 
